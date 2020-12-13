a7ac7d0ce58e14128cb18f0fe9413d78138c5607
DaikiHirose <74400814+DaikiHirose@users.noreply.github.com>
2020-12-13 14:14:45 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,44 @@
-$(document).ready(function() {
-    $('.drawer').drawer();
-  });
+'use strict';
 
-  $('.drawer').drawer({
-    class: {
-      nav: 'drawer-nav',
-      toggle: 'drawer-toggle',
-      overlay: 'drawer-overlay',
-      open: 'drawer-open',
-      close: 'drawer-close',
-      dropdown: 'drawer-dropdown'
-    },
-    iscroll: {
-      // Configuring the iScroll
-      // https://github.com/cubiq/iscroll#configuring-the-iscroll
-      mouseWheel: true,
-      preventDefault: false
-    },
-    showOverlay: true
-  });
+// クリックイベント ここから
+const textBtn = document.getElementById('textBtn');
+const subText = document.getElementById('subText');
+
+subText.style.display = 'none';
+
+function clickBtn() {
+  if (subText.style.display === 'block') {
+    // noneで非表示
+    subText.style.display = 'none';
+    textBtn.value = '続きを読む';
+  } else {
+    // blockで表示
+    subText.style.display = 'block';
+    textBtn.value = '閉じる';
+  }
+}
+// クリックイベント ここまで
+
+// ドロワーメニュー ここから
+$(document).ready(function () {
+  $('.drawer').drawer();
+});
+
+$('.drawer').drawer({
+  class: {
+    nav: 'drawer-nav',
+    toggle: 'drawer-toggle',
+    overlay: 'drawer-overlay',
+    open: 'drawer-open',
+    close: 'drawer-close',
+    dropdown: 'drawer-dropdown'
+  },
+  iscroll: {
+    // Configuring the iScroll
+    // https://github.com/cubiq/iscroll#configuring-the-iscroll
+    mouseWheel: true,
+    preventDefault: false
+  },
+  showOverlay: true
+});
+// ドロワーメニュー ここまで
